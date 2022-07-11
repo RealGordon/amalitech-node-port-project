a8f976506b8e246b7cfd74f2979914abf3ebaca0
RealGordon <92362350+RealGordon@users.noreply.github.com>
2022-07-11 15:23:56 +0100

    updating functions

--- functions/index.js
+++ functions/index.js
@@ -1,122 +1,198 @@
-
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
 // The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
 const functions = require('firebase-functions');
-
+const { Firestore }=require('@google-cloud/firestore')
 // The Firebase Admin SDK to access Firestore.
 const admin = require('firebase-admin');
 const nodemailer = require('nodemailer');
+//const { firestore } = require('firebase-admin');
 admin.initializeApp();
-const PROJECT_BUCKET='amalitech-node-project.appspot.com';
+
+const PROJECT_BUCKET = 'amalitech-node-project.appspot.com';
 exports.createUserDoc = functions.auth.user().onCreate(async (user) => {
-    // [END onCreateTrigger]
-      // [START eventAttributes]
-      const email = user.email; // The email of the user.
-      const displayName = user.displayName; // The display name of the user.
-      // [END eventAttributes]
-      try{
-      await admin.firestore().collection(
-          'users').doc(user.uid.substring(0,10)).set(
-          {
-             name:displayName,
-            email:email,
-            status:true,
-            id:user.uid,
-            test:'local functions'
+  // [END onCreateTrigger]
+  // [START eventAttributes]
+  const email = user.email; // The email of the user.
+  const displayName = user.displayName; // The display name of the user.
+  // [END eventAttributes]
+  try {
+    await admin.firestore().collection(
+      'users').doc(user.uid.substring(0, 10)).set(
+        {
+          name: displayName,
+          email: email,
+          status: false,
+          id: user.uid,
+          test: 'local functions'
         });
-        functions.logger.log('user created success ',email);
-      }
-      catch(e){
-        functions.logger.log('user creation error ',email);
-        functions.logger.log(e);
-      }
-      
-    });
-    //firebase functions:config:set gmail.email="myusername@gmail.com" gmail.password="secretpassword"
-    const gmailEmail =functions.config().gmail.email;
-    const gmailPassword =functions.config().gmail.password;
-    const mailTransport = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: gmailEmail,
-        pass: gmailPassword,
-      },
-    });
-    async function sendEmail(recipientEmail, displayName, APP_NAME,text,attachments) {
-      
-      const mailOptions = {
-          from: `${APP_NAME} <noreply@carddistributer.org>`,
-          to: recipientEmail,
-          //attachments:[{filename:fn||'file.jpg',content:fileBlob}]
-          attachments:attachments
-        };
-      
-        // The user subscribed to the newsletter.
-        mailOptions.subject = text.subject;
-        mailOptions.text = text.message;
-        await mailTransport.sendMail(mailOptions);
-        //functions.logger.log('New welcome email sent to:', recipientEmail);
-        return null;
-      }
-      
-
-// Listens for new messages added to /messages/:documentId/
-// and changes value  at  /messages/:documentId/status
-exports.SendUserEmail = functions.firestore.document('/emailSender/{documentId}')
-.onCreate(async (snap, context) => {
-  // Grab the current value of what was written to Firestore.
-  const data=snap.data();
-  const text = {message:data.message,subject:data.subject};
-  const recipient= data.recipient;
-  const sender=data.sender||'firestore@tester.org';
-  const company=data.company||'card distributer';
-  const files=data.files;
-  const fileAttactments=[];
-
-  const storage=admin.storage();
-  
-  if (files.length<=3){
-  files.forEach((file)=>{
-    const filePath=file.fileId;
-    try{
-   const fileBlob = await storage.bucket(PROJECT_BUCKET).file(
-       filePath).download();
-       fileAttactments.push({content:fileBlob[0],filename:file.filename})
-     }
-     catch(e){
-       functions.logger.log('error occurred while downloading file from gcs ',
-       file.filename);
-     }
-  })}
-  
-  try{
-  await sendEmail(recipient, sender, company,text,fileAttactments);
-  await admin.firestore().collection('emailSender').doc(context.params.documentId)
-  .set({status:'sent'},{merge:true})
-  }catch (e){
-    functions.logger.log('error occurred while sending email for ',recipient);
-    functions.logger.log(e);
-   return  admin.firestore().collection('emailSender').doc(context.params.documentId)
-    .set({status:'error'},{merge:true})
+    functions.logger.log('user created success ', email);
   }
-  // Access the parameter `{documentId}` with `context.params`
-  //functions.logger.log('Uppercasing', context.params.documentId, original);
-  
-  //const uppercase = original.toUpperCase();
-  
-  // You must return a Promise when performing asynchronous tasks inside a Functions
-  // such as
-  // writing to Firestore.
-  // Setting an 'uppercase' field in Firestore document returns a Promise.
-  //return snap.ref.parent.parent.parent.doc()set({uppercase}, {merge: true});
-  
+  catch (e) {
+    functions.logger.log('user creation error ', email);
+    functions.logger.log(e);
+  }
+
 });
+
+
+
+
+// Listens for new messages added to /emailSender/:documentId/
+// and changes value  at  /emailSender/:documentId/status
+exports.SendUserEmail = functions.firestore.document('/emailSender/{documentId}')
+  .onCreate(async (snap, context) => {
+    // Grab the current value of what was written to Firestore.
+    const gmailEmail = functions.config().gmail.email;
+    const gmailPassword = functions.config().gmail.password;
+
+    const emailTransporter = nodemailer.createTransport({
+      host: 'smtp.mail.yahoo.com',
+      port: 465,// or 587
+      service:'yahoo',
+      secure: false,
+      auth: {
+         user: gmailEmail,
+         pass: gmailPassword
+      },
+      debug: false,
+      logger: true 
+    });
+    async function sendEmail(recipientEmail, displayName, APP_NAME, text, attachments) {
+    
+      const mailOptions = {
+        from: gmailEmail,
+        to: recipientEmail,
+        //attachments:[{filename:fn||'file.jpg',content:fileBlob}]
+        attachments: attachments
+      };
+    
+      // The user subscribed to the newsletter.
+      mailOptions.subject = text.subject;
+      mailOptions.text = text.message;
+      await emailTransporter.sendMail(mailOptions);
+      //functions.logger.log('New welcome email sent to:', recipientEmail);
+      return null;
+    }
+    
+    const data = snap.data();
+    const text = { message: data.message, subject: data.subject };
+    const recipient = data.recipient;
+    const sender = data.sender || 'firestore@tester.org';
+    const company = data.company || 'card distributer';
+    const files = data.files;
+    const fileAttactments = [];
+    let attachments;
+    const storage = admin.storage();
+
+    for (const file  of files) {
+    
+        const filePath = file.fileId;
+        try {
+          const fileBlob = await storage.bucket(PROJECT_BUCKET).file(
+            filePath).download();
+          fileAttactments.push({ docId: file.docId, content: fileBlob[0], 
+            filename: file.filename })
+        }
+        catch (e) {
+          functions.logger.log('error occurred while downloading file from gcs ',
+            file.filename);
+        }
+   
+    }
+    const firestore = new Firestore();//  
+    const firestore1=admin.firestore();
+    try {
+      attachments=fileAttactments.map(v=>{
+        const nv={...v};
+        delete nv['docId'];
+        return nv;
+      });
+      await sendEmail(recipient, sender, company, text, attachments);
+      const batch = firestore.batch();
+
+      const emailRef = firestore.doc('emailSender/'+context.params.documentId)
+      batch.update(emailRef, { status: 'sent' })
+
+      fileAttactments.forEach((file) => {
+        const fileRef = firestore.doc('files/'+file.docId);
+        batch.update(fileRef, { emails:Firestore.FieldValue.increment(1) })
+      }
+      )
+      //send notifations
+      await batch.commit()
+    } catch (e) {
+      functions.logger.log('error occurred while sending email for ', recipient);
+      functions.logger.log(e);
+      firestore1.collection('emailSender').doc(context.params.documentId)
+        .set({ status: 'error' }, { merge: true })
+    }
+   
+
+  });
+  
+  
+  exports.newUserActivate = async (event, context) => {
+    //const resource = context.resource;
+     const firestore = new Firestore();
+     const docId=event.value.name.split('/documents/activation/')[1];
+     try{
+     const  oldDoc= await firestore.collection('activationOut').doc(docId).get();
+     const fields=event.value.fields;
+       const userUID=fields.user.stringValue;
+       const operation=fields.o.stringValue;
+       const serial=fields.s.stringValue;
+       const email=fields.email.stringValue;
+       const pos=fields.pos.stringValue;
+     if (oldDoc.exists){
+        const oldDocData=oldDoc.data();
+        if(serial===oldDocData.serial && operation ===oldDocData.operation)return null;
+     }
+     
+       const customClaims={};
+       admin.initializeApp();
+      const  Auth=admin.auth();
+       const userRecord= await Auth.getUser(userUID);
+      if(operation==='activate'){
+        customClaims.status=true;
+        customClaims.pos='user';
+      if( userRecord.customClaims.pos==='user' && userRecord.customClaims.status===true) return null;
+      }
+      else if(operation==='deactivate' && pos==='user'){
+          customClaims.status=false;
+          customClaims.pos='user';
+          if( userRecord.customClaims.pos==='user' && userRecord.customClaims.status===false) return null;
+      }
+      else return null;
+     
+     
+        
+        await Auth.setCustomUserClaims(userUID,customClaims);
+      let date;
+  
+      const userDoc=firestore.collection('users').doc(docId);
+      const sigRef = firestore.collection('activationOut').doc(docId);
+      const batch = firestore.batch();
+      const userRef=firestore.collection('users').doc(docId).
+      collection('activation').doc('history');
+      date=new Date();
+      batch.update(userDoc,{ time: date,r:'success',status:true,serial })
+      .set(sigRef, { time:date,r:'success',s:serial ,e:email,o:operation})
+      .set(userRef, {[operation]:Firestore.FieldValue.arrayUnion(
+        { time: date,r:'success' ,s:serial})},{merge:true});
+       await  batch.commit();
+        console.log(`account ${operation}  for ${email}`);
+      
+        } catch (e) {
+        await firestore.collection('activationOut').doc(docId)
+          .set({o:operation, time: new Date(),r:'fail',s:serial }, { merge: true });
+          console.log(`error occurred while ${operation} account for ${email}`);
+          console.log(e)
+        }
+        return null;
+    
+  
+  
+  };
+
 async function downloadIntoMemory() {
   // Downloads the file into a buffer in memory.
   const contents = await storage.bucket(bucketName).file(fileName).download();
